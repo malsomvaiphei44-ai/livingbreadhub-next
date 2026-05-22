b28fd57e43917c8bec7feae2da749d1efec437fb
malsomvaiphei44-ai <malsomvaiphei44@gmail.com>
2026-05-22 20:27:18 +0530

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
       messages[messages.length - 1]?.content || "";
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash-latest",
+      model: "gemini-pro",
     });
 
     const result = await model.generateContent(lastMessage);
